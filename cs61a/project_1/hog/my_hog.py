53f1f6299f04b2432617d897dd54c34f77b4e7c0
kravetsd <kdykrg@gmail.com>
2016-10-20 08:53:41 -0400

    Added hog files from project_1 of cs61a_sprin2015 and my_hog.py in which i added three answered questions

---
+++ cs61a/project_1/hog/my_hog.py
@@ -0,0 +1,52 @@
+# coding: utf-8
+from dice import four_sided, six_sided, make_test_dice
+from ucb import main, trace, log_current_line, interact
+def roll_dice(num_rolls, dice=six_sided):
+    """Simulate rolling the DICE exactly NUM_ROLLS times. Return the sum of
+    the outcomes unless any of the outcomes is 1. In that case, return 0.
+    """
+    # These assert statements ensure that num_rolls is a positive integer.
+    assert type(num_rolls) == int, 'num_rolls must be an integer.'
+    assert num_rolls > 0, 'Must roll at least once.'
+    # BEGIN Question 1
+    
+    sum = 0
+    for outcome in range(num_rolls):
+        a = dice()
+        if a == 1:
+            print('sorry it outcomes {}'.format(a))
+            return 0
+        else: sum = sum + a
+    return sum
+    # END Question 1
+def take_turn(num_rolls, opponent_score, dice=six_sided):
+    """Simulate a turn rolling NUM_ROLLS dice, which may be 0 (Free Bacon).
+
+    num_rolls:       The number of dice rolls that will be made.
+    opponent_score:  The total score of the opponent.
+    dice:            A function of no args that returns an integer outcome.
+    """
+    assert type(num_rolls) == int, 'num_rolls must be an integer.'
+    assert num_rolls >= 0, 'Cannot roll a negative number of dice.'
+    assert num_rolls <= 10, 'Cannot roll more than 10 dice.'
+    assert opponent_score < 100, 'The game should be over.'
+     # BEGIN Question 2
+    strscore = str(opponent_score)
+    if num_rolls == 0:
+        try:
+            return int(strscore[0])+int(strscore[1])
+        except: return opponent_score
+    else:
+        return roll_dice(num_rolls,dice)
+       
+    # END Question 2
+def select_dice(score, opponent_score):
+    """Select six-sided dice unless the sum of SCORE and OPPONENT_SCORE is a
+    multiple of 7, in which case select four-sided dice (Hog wild).
+    """
+    # BEGIN Question 3
+    if (score*opponent_score)%7 ==0:
+        return four_sided
+    else:
+        return six_sided
+    # END Question 3
