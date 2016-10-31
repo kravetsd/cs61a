5d5f0318205d05ac3b452d19fcebd5178642e3bd
kravetsd <kdykrg@gmail.com>
2016-10-31 13:48:11 -0400

    added answer for questions up to 7-th

---
+++ cs61a/lab_2/condition.py
@@ -0,0 +1,34 @@
+# coding: utf-8
+def count_cond(condition):
+    """Returns a function with one parameter N that counts all the numbers from
+    1 to N that satisfy the two-argument predicate function CONDITION.
+
+    >>> count_factors = count_cond(lambda n, i: n % i == 0)
+    >>> count_factors(2)   # 1, 2
+    2
+    >>> count_factors(4)   # 1, 2, 4
+    3
+    >>> count_factors(12)  # 1, 2, 3, 4, 6, 12
+    6
+
+    >>> is_prime = lambda n, i: count_factors(i) == 2
+    >>> count_primes = count_cond(is_prime)
+    >>> count_primes(2)    # 2
+    1
+    >>> count_primes(3)    # 2, 3
+    2
+    >>> count_primes(4)    # 2, 3
+    2
+    >>> count_primes(5)    # 2, 3, 5
+    3
+    >>> count_primes(20)   # 2, 3, 5, 7, 11, 13, 17, 19
+    8
+    """
+    def cond_term(x):
+        i, count = 1,0
+        while i <= x:
+            if condition(x,i):
+                count +=1
+            i+=1
+        return count
+    return cond_term
