2d9d9868527a8ed66b29a165c6d56de88e214667
kravetsd <kdykrg@gmail.com>
2016-11-01 13:54:15 -0400

    added lab2 question 8 answer - Question 8: I Heard You Liked Functions...

--- cs61a/lab_2/lab_2.py
+++ cs61a/lab_2/lab_2.py
@@ -46,3 +46,45 @@ def compose1(f, g):
     108
     """
     return lambda x: f(g(x))
+def cycle(f1,f2,f3):
+    """Returns a function that is itself a higher-order function.
+
+    >>> def add1(x):
+    ...     return x + 1
+    >>> def times2(x):
+    ...     return x * 2
+    >>> def add3(x):
+    ...     return x + 3
+    >>> my_cycle = cycle(add1, times2, add3)
+    >>> identity = my_cycle(0)
+    >>> identity(5)
+    5
+    >>> add_one_then_double = my_cycle(2)
+    >>> add_one_then_double(1)
+    4
+    >>> do_all_functions = my_cycle(3)
+    >>> do_all_functions(2)
+    9
+    >>> do_more_than_a_cycle = my_cycle(4)
+    >>> do_more_than_a_cycle(2)
+    10
+    >>> do_two_cycles = my_cycle(6)
+    >>> do_two_cycles(1)
+    19
+    """
+    def func2(n):
+        def func3(x):
+            i,count = 1,0
+            flst = [f1,f2,f3]
+            if n == 0:
+                return x
+            else:
+                while i<=n:
+                    if count > len(flst)-1:
+                        count = 0
+                    x = flst[count](x)
+                    count += 1
+                    i += 1
+            return x
+        return func3
+    return func2
